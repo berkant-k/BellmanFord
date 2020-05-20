3a4e039131b97bd4dc83e08441262185d0bc73ae
berkant <berkant@kardelenyazilim.com>
2020-05-20 11:36:36 +0300

    DraftPrint added

--- BellmanFordApp/ShortestPath.cs
+++ BellmanFordApp/ShortestPath.cs
@@ -16,12 +16,20 @@ namespace BellmanFord
             source.Distance = 0;
             for (int i = 0; i < graph.Vertices.Count; i++)
             {
+                PrintDraft(graph, i + 1);//Just for testing 
                 foreach (var edge in graph.Edges)
                 {
                     edge.Relax();
                 }
             }
             return !graph.Edges.Any(p => p.Destination.Distance > p.Source.Distance + p.Weight);     //negative cycle control
+        }
+
+        private static void PrintDraft(Graph graph, int iteration)
+        {
+            Console.WriteLine($"Iteration={iteration}");
+            Console.WriteLine("-------------");
+            graph.Print();
         }
 
     }
